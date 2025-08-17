99cea61eeec10263c4562e5f2e02b462afa7e495
Yamoltdinov Oleg <YamoltdinovOleg@yandex.com>
2025-08-17 18:57:15 +0300

    .

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .models import Task
-from .serializers import TaskSerializer, TaskListSerializer
+from .serializers import TaskListSerializer, TaskSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
