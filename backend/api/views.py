c0e742654c9cb3a70ce291b4e9dbe6ce0f6db915
Yamoltdinov Oleg <YamoltdinovOleg@yandex.com>
2025-08-18 08:34:36 +0300

    ю

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,15 @@
-"""Views for Task API."""
-
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from .models import Task
-from .serializers import TaskListSerializer, TaskSerializer
+from .serializers import TaskSerializer
 
 
-class TaskViewSet(viewsets.ModelViewSet):
-    """ViewSet for Task model."""
-
-    queryset = Task.objects.all()
+class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 
-    def list(self, request):
-        """List all tasks with simplified serializer."""
-        queryset = self.get_queryset()
-        serializer = TaskListSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def destroy(self, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object())
+        super().destroy(*args, **kwargs)
+        return Response(serializer.data, status=status.HTTP_200_OK)
